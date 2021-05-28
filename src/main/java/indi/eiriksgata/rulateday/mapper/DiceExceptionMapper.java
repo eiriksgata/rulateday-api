99da834394a51bd28acfca6e0d7ffe84ff350545
Eiriksgata <2353686862@qq.com>
2021-05-28 17:39:13 +0800

    Fix mybatis plus attribute config

--- src/main/java/indi/eiriksgata/rulateday/mapper/DiceExceptionMapper.java
+++ src/main/java/indi/eiriksgata/rulateday/mapper/DiceExceptionMapper.java
@@ -2,8 +2,11 @@ package indi.eiriksgata.rulateday.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import indi.eiriksgata.rulateday.pojo.DiceException;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -14,6 +17,7 @@ import java.util.List;
  * date: 2021/5/20
  **/
 @Mapper
+@Repository
 public interface DiceExceptionMapper extends BaseMapper<DiceException> {
 
     @Select("select * from dice_exception")
